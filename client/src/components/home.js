a1d420c2b3b6d2a3e790b637d0237759fc81f2a5
William Myers <williamrmyers@gmail.com>
2018-09-11 13:45:40 -0700

    Added GET calls for /customers, /members,  to front / route. Added POST, PATCH and DELETE methods.

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -4,13 +4,15 @@ import axios from 'axios';
 class Home extends React.Component {
   state = {
       token: undefined,
-      appointments: []
+      appointments: [],
+      messages: [],
+      customers: []
   };
-
-  getAppointments = () => {
-    const authHeaders = {
-      headers: {'x-auth': this.props.token }
-    };
+// Appointments handelers
+getAppointments = () => {
+  const authHeaders = {
+    headers: { 'x-auth': this.props.token }
+  };
   axios.get('/appointments', authHeaders)
     .then((res) => {
       this.setState(() => ({ appointments: res.data.appointments }))
@@ -20,9 +22,89 @@ class Home extends React.Component {
   });
 }
 
+
+postAppointments = () => {
+  const authHeaders = {
+    headers: { 'x-auth': this.props.token }
+  };
+  const body = {
+  	"message":"Meet with William",
+  	"date": 1536465944,
+  	"customer": "5b92f34860687de21bcdbea4"
+  }
+
+  axios.post('/appointment', body, authHeaders).then((res)=>{
+      this.getAppointments();
+  }).catch((error)=>{
+    console.log(error);
+  });
+}
+
+patchAppointments = (id, name) => {
+  const authHeaders = {
+    headers: { 'x-auth': this.props.token }
+  };
+  const body = {
+  	"message":`Meet with ${name}`,
+  	"date": 1536465944,
+  	"customer": "5b92f34860687de21bcdbea4"
+  }
+
+  axios.patch(`/appointment/${id}`, body, authHeaders).then((res)=>{
+      this.getAppointments();
+  }).catch((error)=>{
+    console.log(error);
+  });
+}
+
+deleteAppointments = (id) => {
+  const authHeaders = {
+    headers: { 'x-auth': this.props.token }
+  };
+  axios.delete(`/appointment/${id}`, authHeaders).then((res)=>{
+      this.getAppointments();
+  }).catch((error)=>{
+    console.log(error);
+  });
+}
+
+handleDelete = (e) => {
+  const id = e.target.id;
+  this.deleteAppointments(id);
+}
+
+getMessages = () => {
+  const authHeaders = {
+    headers: { 'x-auth': this.props.token }
+  };
+  axios.get('/messages', authHeaders)
+    .then((res) => {
+      this.setState(() => ({ messages: res.data.messages }))
+    })
+    .catch((error) => {
+      console.log(error);
+  });
+}
+
+getCustomers = () => {
+  const authHeaders = {
+    headers: { 'x-auth': this.props.token }
+  };
+
+  axios.get('/customers', authHeaders)
+    .then((res) => {
+      this.setState(() => ({ customers: res.data.customers }))
+    })
+    .catch((error) => {
+      console.log(error);
+  });
+}
+
   componentWillMount() {
     if (this.props.isAuthenticated) {
       this.getAppointments();
+      this.getMessages();
+      this.getCustomers();
     }
   }
 
@@ -37,8 +119,9 @@ class Home extends React.Component {
                   <div className="container has-text-centered content">
                     <h3>Appointments</h3>
                       <ul>
-                        {this.state.appointments.map(appointment => (<li key={appointment._id}><p>{appointment.message} at {appointment.date}</p></li>))}
+                        {this.state.appointments.map(appointment => (<li key={appointment._id}><p>{appointment.message} at {appointment.date}<button id={appointment._id} onClick={this.handleDelete}>Delete</button></p></li>))}
                       </ul>
+                      <button onClick={this.postAppointments}>Add Appointment</button>
                   </div>
                 </div>
               </div>
