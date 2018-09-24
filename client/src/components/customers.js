39a38365ede6675486b46b843d04260da42f2a4e
William Myers <williamrmyers@gmail.com>
2018-09-23 19:08:53 -0700

    Wired up add contact form, and added toggle form button.

--- client/src/components/customers.js
+++ client/src/components/customers.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { Container, Segment } from 'semantic-ui-react'
+import { Container, Segment, Input, Header, Form } from 'semantic-ui-react'
 import axios from 'axios';
 
 class Customers extends React.Component {
 
   state = {
-    customers: []
+    customers: [],
+    contactFormVisible: false
+  }
+
+  postCustomer = (body) => {
+    const authHeaders = {
+      headers: { 'x-auth': this.props.token }
+    };
+
+    axios.post('/customer', body, authHeaders).then((res)=>{
+        this.getCustomers();
+    }).catch((error)=>{
+      this.error(error);
+    });
+  }
+
+  handelPostCustomer = (e) => {
+    const el = e.target.elements;
+    const firstName = el.firstName.value.trim();
+    const lastName = el.lastName.value.trim();
+    const phone = el.phone.value.trim();
+    const email = el.email.value.trim();
+    if (phone) {
+      this.postCustomer({first_name: firstName, last_name: lastName, phone, email});
+      el.firstName.value = '';
+      el.lastName.value = '';
+      el.phone.value = '';
+      el.email.value = '';
+    }
   }
 
   getCustomers = () => {
@@ -21,26 +49,47 @@ class Customers extends React.Component {
         console.log(error);
     });
   }
+  toggleAddForm = (state) => {
+    this.setState(() => ({ contactFormVisible: this.state.contactFormVisible ? false : true }))
+  }
 
   componentWillMount() {
-    this.getCustomers()
+    this.getCustomers();
   }
 
   render() {
     return (
       <div>
+      <Container text>
+        {this.state.contactFormVisible ? (
+        <div>
+          <h2>Add Contact</h2>
+          <Form onSubmit={this.handelPostCustomer}>
+            <Form.Group widths='equal'>
+              <Form.Input fluid name='firstName' label='First name' placeholder='First name' />
+              <Form.Input fluid name='lastName' label='Last name' placeholder='Last name' />
+            </Form.Group>
+
+            <Form.Group widths='equal'>
+              <Form.Input fluid name='phone' type='phone' label='Phone' placeholder='Phone' />
+              <Form.Input fluid name='email' type='email' label='Email'placeholder='Email' />
+            </Form.Group>
+            <Form.Button>Submit</Form.Button>
+          </Form>
+        </div>
+        ): false}
+
         <h1>Contacts</h1>
         <Segment.Group raised>
-          {this.state.customers.map(customer => (<Segment key={customer._id}>{customer.first_name} {customer.last_name} {customer.phone}</Segment>))}
+          {this.state.customers.map(customer => (<Segment key={customer._id}>{customer.first_name} {customer.last_name}  {customer.phone}</Segment>))}
         </Segment.Group>
+    </Container>
+      <div className='addButtom'>
+        <button onClick={this.toggleAddForm}>Add</button>
       </div>
+    </div>
     );
   }
 }
 
 export default Customers;
-
-
-// <ul>
-//   {this.state.customers.map(customer => (<li key={customer._id}>{customer.first_name} {customer.last_name} Number: {customer.phone}</li>))}
-// </ul>
